ef6284db816e4cb8169d114ea0e046a7973afbd3
Eduardo Ramírez <ramirezcota.eduardo@gmail.com>
2018-11-11 18:45:19 -0700

    n

---
+++ src/java/Prueba.java
@@ -0,0 +1,21 @@
+
+import dao.Clientes;
+import dao.Conexion;
+import interfaces.IPersistencia;
+import java.util.ArrayList;
+import objetosNegocio.Cliente;
+import persistencia.PersistenciaBD;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Eduardo Ramírez
+ */
+public class Prueba {
+    
+}
